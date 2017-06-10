680ca8eac2aea779fe9f5537e0218a43f96dfc42
John Huang <ymhuang@fmbase.tw>
2017-06-10 13:18:46 +0800

    Add indexes in epa_datasets and lass_datasets tables in two database migrations

---
+++ database/migrations/2017_06_10_051139_add_indexes_to_lass_datasets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIndexesToLassDatasetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('lass_datasets', function (Blueprint $table) {
+           $table->index('published_datetime');
+           $table->unique(['site_id', 'published_datetime']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('lass_datasets', function (Blueprint $table) {
+           $table->dropIndex('published_datetime');
+           $table->dropUnique(['site_id', 'published_datetime']);
+        });
+    }
+}
---
+++ database/migrations/2017_06_10_051712_add_index_to_epa_datasets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIndexToEpaDatasetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('epa_datasets', function (Blueprint $table) {
+           $table->index('published_datetime');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('epa_datasets', function (Blueprint $table) {
+            $table->dropIndex('published_datetime');
+        });
+    }
+}
